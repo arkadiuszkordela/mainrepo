6368765b49273e3ccd902bddfcc0567c5e61b378
arkadiuszkordela <arkadiuszkordela@tutanota.com>
2023-02-03 12:54:48 +0100

    Update exercise_1.py
    First steps.

--- git_python_udemy/exercise_1.py
+++ git_python_udemy/exercise_1.py
@@ -1,1 +1,42 @@
-# There will be my first Udemy's exercise make with GitHub.
+# It is a exercise where I have to point the lists with a least one even number.
+
+number_list_1 = [3, 5, 10, 13, 17]
+number_list_2 = [3, 7, 13, 15, 23]
+
+def even_number(list):
+    for number in list:
+        if number % 2 == 0:
+            return True
+    
+    return False
+
+print(even_number(number_list_1))
+print(even_number(number_list_2))
+### Method 1. /\
+
+numbers_1 = [number % 2 == 0
+for number in number_list_1]
+numbers_2 = [number % 2 == 0
+for number in number_list_2]
+
+print(any(numbers_1))
+print(any(numbers_2))
+### Method 2. /\
+
+def eve_nr(list):
+    return any([number % 2 == 0 for number in list])
+### Method 3. /\
+
+# In this exercise I have to point who from that two persons below can Python and JavaScript.
+
+john = {
+    'name': 'John Doe',
+    'age': 30,
+    'skills': ['Python', 'JavaScript', 'C++']
+}
+
+jane = {
+    'name': 'Jane Smith',
+    'age': 25,
+    'skills': ['Python', 'Java']
+}
