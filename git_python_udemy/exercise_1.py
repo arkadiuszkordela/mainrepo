92a12a21f46262ac81e2f52be99d72b973583f30
arkadiuszkordela <arkadiuszkordela@tutanota.com>
2023-02-03 17:30:16 +0100

    Update exercise_1.py
    Next step.

--- git_python_udemy/exercise_1.py
+++ git_python_udemy/exercise_1.py
@@ -1,7 +1,7 @@
 # It is a exercise where I have to point the lists with a least one even number.
 
 print(' ')
-print('*************************** E1 M1')
+print('********** M1')
 print(' ')
 
 number_list_1 = [3, 5, 10, 13, 17]
@@ -41,61 +41,3 @@ def eve_nr(list):
 print(eve_nr(number_list_1))
 print(eve_nr(number_list_2))
 ### Method 3. /\
-
-# In this exercise I have to point who from that two persons below can Python and JavaScript.
-
-print(' ')
-print('*************************** E2 M1')
-print(' ')
-
-john = {
-    'name': 'John Doe',
-    'age': 30,
-    'skills': ['Python', 'JavaScript', 'C++']
-}
-
-jane = {
-    'name': 'Jane Smith',
-    'age': 25,
-    'skills': ['Python', 'Java']
-}
-
-def skills(list):
-    for value in list:
-        if value == 'Python':
-            print('Python ok!')
-        elif value == 'JavaScript':
-            print('JavaScript ok!')
-        elif value != 'Python':
-            print('No matter skill.')
-        elif value != 'JavaScript':
-            print('No matter skill.')
-        elif value == 'C++':
-            print('No matter skill.')
-
-print('John:')
-print(skills(john['skills']))
-
-print(' ')
-
-print('Jane:')
-print(skills(jane['skills']))
-### Method 1. /\
-
-print(' ')
-print('********** M2')
-print(' ')
-        
-def check_skills(person, skills):
-    return all(skill in person['skills'] for skill in skills)
-
-required_skills = ['Python', 'JavaScript']
-
-print('John:')
-print(check_skills(john, required_skills))
-
-print(' ')
-
-print('Jane:')
-print(check_skills(jane, required_skills))
-### Method 2. /\
