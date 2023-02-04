b9b33ea1dd822b12cb88cd092b2b272db08c365a
arkadiuszkordela <arkadiuszkordela@tutanota.com>
2023-02-04 18:48:47 +0100

    Create exercise_4.py
    New exercise.

---
+++ git_python_udemy/exercise_4.py
@@ -0,0 +1,11 @@
+# In this exercise I have to make a two lists with five cards from a set.
+
+import random
+
+cardList = ["9", "9", "9", "9",
+            "10", "10", "10", "10",
+            "Jack", "Jack", "Jack", "Jack",
+            "Queen", "Queen", "Queen", "Queen",
+            "King", "King", "King", "King",
+            "Ace", "Ace", "Ace", "Ace",
+            "Joker", "Joker"]
