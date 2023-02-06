59f7effa33d8c4fa94e7754529074b35f57abe95
arkadiuszkordela <arkadiuszkordela@tutanota.com>
2023-02-06 15:57:45 +0100

    Update exercise_6.py

--- git_python_udemy/exercise_6.py
+++ git_python_udemy/exercise_6.py
@@ -1,1 +1,5 @@
-# In this exercise I have to load names and surnames from a file.
+# In this exercise I have to load names and surnames from a file. Nest I have to separate names and surnames and save them in different files.
+
+with open('names.txt', 'r', encoding = 'UTF-8') as file:
+          for line in file:
+                  print(line)
