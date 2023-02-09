697964256a086926833ab72948ce8bcb2977e64e
arkadiuszkordela <arkadiuszkordela@tutanota.com>
2023-02-09 13:06:50 +0100

    Create exercise_14.py

---
+++ git_python_udemy/exercise_14.py
@@ -0,0 +1,24 @@
+# In this exercise I have to create five rockets whose will move up randomly with randomly speed.
+
+from random import randint
+
+class Rocket:
+    
+    def __init__(self):
+        self.altitude = 0
+        
+        random_speed = randint(1, 7)
+        self.speed = random_speed
+        
+    def move_up(self):
+        self.altitude += self.speed
+
+rockets = [Rocket() for _ in range(5)]
+
+for _ in range(10):
+    rocket_to_move = randint(0, 4)
+    rockets[rocket_to_move].move_up()
+
+for rocket in rockets:
+    print(rocket.altitude)
+
