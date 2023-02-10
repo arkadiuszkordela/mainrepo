42f3aa632b77790eaebc4657ddfa660857ef99d3
arkadiuszkordela <arkadiuszkordela@tutanota.com>
2023-02-10 16:15:15 +0100

    Add files

---
+++ git_python_udemy/exercise_15.py
@@ -0,0 +1,12 @@
+import Bank
+
+print('Welcome in a bank application.')
+print(' ')
+in_account_number = int(input('Enter a account number: '))
+in_owner_name = str(input('Enter a name of a owner: '))
+
+account = Bank(account_number = in_account_number, owner_name = in_owner_name)
+
+account.deposit(int(input('Enter how many money do you want to deposit: ')))
+account.withdraw(int(input('Enter how many money do you want to withdraw: ')))
+account.get_balance()
